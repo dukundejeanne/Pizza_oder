303c37fa2aa1a60ec32a8c4120f835c2557c6fec
Jeannne Dukunde <dukundem90@gmail.com>
2019-08-01 17:35:47 +0200

    add form

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $("#button, #order").click(function(){
+//         $(".btn").toggle();
+//         $("#order").toggle();
+//     });
+//    });
+   $(document).ready(function(){
+    $("#button, #one").click(function(){
+        $(".btn btn-success").toggle();
+        $("#one").toggle();
+    });
+   });
